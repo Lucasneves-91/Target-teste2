f1bd4345db783834413e7ea736466395ca6955b8
Lucas Neves <lucas.ccdr@gmail.com>
2024-09-10 19:36:49 -0300

    Criada a interação com o usuario.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int numero;
+
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite um número para saber se ele faz parte dos números Fibonacci:");
+        numero = leitura.nextInt();
+
+
     }
 }
